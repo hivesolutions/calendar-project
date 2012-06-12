99d4b593d74ef3b4beadd543e0f16707f87c6631
regadas <filiperegadas@gmail.com>
2012-06-12 17:55:40 +0100

    using django-registration to speed up user registration dev time

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'home.views.index', name="index"),
-    url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'home/login.html'}),
 )
